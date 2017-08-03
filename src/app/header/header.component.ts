d17ee6d7f9b38a59ba53d070be460607509d2206
agirmar92 <aegir13@ru.is>
2017-08-03 14:17:48 +0000

    header graphic now has responsive sizing

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,8 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  @ViewChild('kritHeader') kritHeader: ElementRef;
+
+  backgroundSize: string;
+
   constructor() { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.backgroundSize = this.calculateBackgroundSize();
+  }
+
+  @HostListener('window:resize', []) onScrollEvent() {
+    this.backgroundSize = this.calculateBackgroundSize();
+  }
+
+  calculateBackgroundSize(): string {
+    const el = this.kritHeader.nativeElement;
+    const newWidth = el.clientWidth;
+    const newHeight = el.clientHeight;
+    const pictureTooLarge = (newWidth / newHeight) > 1.75;
+    if (pictureTooLarge) {
+      return (newHeight * 1.22) + 'px';
+    } else {
+      return '70%';
+    }
+  }
 
 }
