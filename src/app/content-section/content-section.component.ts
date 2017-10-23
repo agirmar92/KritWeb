6fa13df22a45a791e3cc549673d0f55ca5cd964b
agirmar92 <aegir13@ru.is>
2017-10-23 11:29:54 +0000

    fix for scroll top bug

--- src/app/content-section/content-section.component.ts
+++ src/app/content-section/content-section.component.ts
@@ -23,8 +23,10 @@ export class ContentSectionComponent implements OnInit {
   @HostListener('window:scroll', ['$event']) onScrollEvent($event) {
     const el = this.contentSection.nativeElement;
     const parent = this.contentSection.nativeElement.offsetParent;
+    const scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset :
+      (document.documentElement || document.body.parentNode || document.body);
 
-    if (el.offsetTop <= parent.scrollTop && (el.offsetTop + el.clientHeight * 2) > parent.scrollTop) {
+    if (el.offsetTop <= (parent.scrollTop || scrollTop) && (el.offsetTop + el.clientHeight * 2) > (parent.scrollTop || scrollTop)) {
       this.topContent.emit(this.contentName);
     }
   }
